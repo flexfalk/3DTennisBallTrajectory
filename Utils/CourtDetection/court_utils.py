7e805fd4bb9f47a241388fb0fcaab5956a54b7a9
mert <mert@itu.dk>
2024-04-16 14:31:41 +0200

    Update court utils

--- Utils/CourtDetection/court_utils.py
+++ Utils/CourtDetection/court_utils.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+# Add the parent directory to sys.path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+sys.path.insert(1, parent_dir)
+
+
 import cv2
 import numpy as np
 from scipy.spatial import distance
